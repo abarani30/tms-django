412ceeb4904fc5b2c844cc15cb265393cb65c400
abarani30 <alisattarbarani@outlook.com>
2022-10-26 12:52:30 +0300

    ali

--- myapp/tasks/views/systems.py
+++ myapp/tasks/views/systems.py
@@ -1,7 +1,9 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.models import User
 from myapp.models import Task
+import datetime 
 
 class SystemTasksView(View):
   template_name = "systems_tasks.html"
@@ -18,4 +20,23 @@ class SystemTasksView(View):
     return [user for user in User.objects.all().select_related("profile") if user.profile.division == "الانظمة" and user.is_active]
 
   def getAllTasks(self):
-    return [task for task in Task.objects.prefetch_related("employees").all().order_by("-created_at") if task.user.profile.division == "الانظمة"]
+    return [
+      task 
+      for task in Task.objects.prefetch_related("employees")
+      .filter(start_date__range=(self.getFirstDayOfMonth(), self.getLastDayOfMonth()))
+      .order_by("-created_at") 
+      if task.user.profile.division == "الانظمة" 
+    ]
+
+  def getFirstDayOfMonth(self):
+    return str(datetime.date.today().replace(day=1))
+  
+  def getLastDayOfMonth(self):
+    return str(datetime.date.today().replace(day=31))
+
+  
+  def post(self, request):
+    if (request.META.get("HTTP_X_CSRFTOKEN") != request.POST.get("csrftoken")):
+      return JsonResponse({"errMsg": "Invalid request"})
+    print("You hit save button through your template")
+    return JsonResponse({"msg": "success"})
---
+++ static/js/task/add_task.js
@@ -0,0 +1,37 @@
+var taskForm        = document.getElementById("task-form");
+var csrf            = document.getElementsByName("csrfmiddlewaretoken");
+var taskSubject     = document.getElementById("task-subject");
+var startDate       = document.getElementById("start-date");
+var endDate         = document.getElementById("end-date");
+var taskEmployees   = document.querySelectorAll("input[id='employees']");
+
+
+if (taskForm !== null) {
+    taskForm.addEventListener("submit", (e)=> {
+        e.preventDefault();
+ 
+        var employees = [];
+        for (let i = 0; i < taskEmployees.length; i++) {
+            if (taskEmployees[i].checked) employees.push(taskEmployees[i].value);
+        }
+
+        const formData = new FormData();
+        formData.append('csrftoken', csrf[0].value);
+
+        $.ajax({
+            url: '/tasks/systems/',
+            type: "POST",
+            enctype: "multipart/form-data",
+            headers: {
+                "X-Requested-With": "XMLHttpRequest",
+                "X-CSRFToken": csrf[0].value,  
+            },
+            mode: "same-origin",
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false
+        })
+
+    })
+}
